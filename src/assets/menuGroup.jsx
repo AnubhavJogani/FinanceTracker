1b5d59ac18fabb9515013b5ac4a7dbcf2c8d9d99
Anubhav <anubhavjogani@gmail.com>
2026-03-13 22:14:48 +0530

    add lucid icon and working on sidebar

---
+++ src/assets/menuGroup.jsx
@@ -0,0 +1,29 @@
+export const menuGroups = [
+    {
+        title: "General",
+        items: [
+            { name: "Dashboard", icon: "LayoutDashboard" },
+            { name: "All Expenses", icon: "Receipt" },
+            { name: "Bill & Subscription", icon: "FileText" },
+            { name: "Investment", icon: "TrendingUp" },
+            { name: "Card", icon: "CreditCard" },
+            { name: "Goals", icon: "Target" },
+        ],
+    },
+    {
+        title: "Tools",
+        items: [
+            { name: "Insight", icon: "Lightbulb" },
+            { name: "Analytics", icon: "BarChart3" },
+        ],
+    },
+    {
+        title: "Other",
+        items: [
+            { name: "Setting", icon: "asdf"},
+            { name: "Help Center", icon: "testr"},
+            { name: "Support", icon: "asdf"},
+            { name: "Logout", icon: "Asdf"}
+        ]
+    }
+];
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { Scale } from 'lucide-react';
+import { menuGroups } from '../assets/menuGroup';
 
 const SideBar = () => {
   return (
-      <div className='flex flex-col w-60 bg-gray-200'>SideBar</div>
+      <div className='flex flex-col justify-between w-60 bg-gray-200 p-2'>
+        <div className='flex flex-row items-center gap-2'>
+          <Scale size={40} color= '#00ad14'/>
+          <h1 className='text-xl font-black'>Expensify</h1>
+        </div>
+        <div className='flex flex-col'>
+          {
+            menuGroups.map( item => (
+              <div className='flex flex-col gap-2'>
+                <div>{item.title}</div>
+                <div className='flex flex-col p-2 gap-1'>
+                  {item.items.map(ele => (
+                    <div>{ele.name}</div>
+                  ))}
+                </div>
+                <hr />
+              </div>
+            ))
+          }
+        </div>
+        <div>
+          card
+        </div>
+      </div>
   )
 }
 
